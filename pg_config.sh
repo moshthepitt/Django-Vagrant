6ddb87481a937a5fe3c59e35bea9ad1fa414a7d1
kelvin <kelvin@jayanoris.com>
2015-02-26 00:51:07 +0300

    initial commit of vagrant files

---
+++ pg_config.sh
@@ -0,0 +1,8 @@
+apt-get -qqy update
+apt-get -qqy install python-dev python-pip
+apt-get -qqy install libpq-dev postgresql
+apt-get -qqy install libjpeg8-dev libfreetype6-dev
+apt-get -qqy install redis-server git
+apt-get -qqy install build-essential 
+pip install virtualenvwrapper
+apt-get -qqy update
