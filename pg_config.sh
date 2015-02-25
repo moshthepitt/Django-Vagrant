a2102fccec1e9e6f9234a826965f4342cffa95d9
kelvin <kelvin@jayanoris.com>
2015-02-26 01:27:39 +0300

    adding libmysqlclient-dev to shell provisioner

--- pg_config.sh
+++ pg_config.sh
@@ -3,6 +3,6 @@ apt-get -qqy install python-dev python-pip
 apt-get -qqy install libpq-dev postgresql
 apt-get -qqy install libjpeg8-dev libfreetype6-dev
 apt-get -qqy install redis-server git
-apt-get -qqy install build-essential 
+apt-get -qqy install build-essential libmysqlclient-dev
 pip install virtualenvwrapper
 apt-get -qqy update
